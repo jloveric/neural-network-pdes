47d32db1404c7a474caa0c01a34af065f1780f68
John <john.loverich@gmail.com>
2022-05-27 18:03:15 -0700

    Adding test directory and dummies

---
+++ tests/test_loss.py
@@ -0,0 +1,13 @@
+import os
+import pytest
+from examples.euler import (
+    euler_loss,
+    interior_loss,
+    left_dirichlet_bc_loss,
+    right_dirichlet_bc_loss,
+)
+
+
+def test_poly_convolution_2d_produces_correct_sizes():
+
+    assert True is True
