Adding hessian calculation and fixing normalize

neural_network_pdes/euler_conservative.py
@@ -16,7 +16,7 @@ def pde_grid():
 
 
 class PDEDataset(Dataset):
-    def __init__(self, size: int = 10000, rotations: int = 1, gamma:float=1.4):
+    def __init__(self, size: int = 10000, rotations: int = 1, gamma: float = 1.4):
 
         self._gamma = gamma
 
@@ -46,9 +46,8 @@ class PDEDataset(Dataset):
         p = torch.where(x < 0, 1.0, 0.1)
         u = torch.where(x < 0, 0.0, 0.0)
 
-        mv = rho*u
-        energy =   (p/(self._gamma-1.0)) +0.5*mv*mv/rho
-
+        mv = rho * u
+        energy = (p / (self._gamma - 1.0)) + 0.5 * mv * mv / rho
 
         self.input = torch.cat([x, t], dim=1)
         self.output = torch.cat([rho, mv, energy], dim=1)
@@ -77,38 +76,46 @@ def right_dirichlet_bc_loss(outputs: Tensor, targets: Tensor):
     diff = targets - outputs
     return torch.dot(diff.flatten(), diff.flatten())
 
-def pressure(q:Tensor, gamma:float) :
-    rho = q[:,0]
-    mx = q[:,1]
-    en = q[:,2]
 
-    p = (gamma-1.0)*(en-0.5*(mx*mx/rho))
+def pressure(q: Tensor, gamma: float):
+    rho = q[:, 0]
+    mx = q[:, 1]
+    en = q[:, 2]
+
+    p = (gamma - 1.0) * (en - 0.5 * (mx * mx / rho))
     return p
 
-def flux(q:Tensor, gamma: float=1.4) :
 
+def flux(q: Tensor, gamma: float = 1.4):
 
     p = pressure(q=q, gamma=gamma)
-    rho = q[:,0]
-    mx = q[:,1]
+    rho = q[:, 0]
+    mx = q[:, 1]
     en = q[:, 2]
 
     f = torch.zeros_like(q, requires_grad=True)
     with torch.no_grad():
-        f[:,0] = mx
-        f[:,1] = (mx*mx/rho)+p
-        f[:,2] = mx*(en+p)/rho
+        f[:, 0] = mx
+        f[:, 1] = (mx * mx / rho) + p
+        f[:, 2] = mx * (en + p) / rho
     return f
 
-    
 
-def interior_loss(q: Tensor, grad_q: Tensor, grad_f: Tensor):
+def interior_loss(
+    q: Tensor,
+    grad_q: Tensor,
+    grad_f: Tensor,
+    hessian: Tensor,
+    artificial_viscosity: float = 0,
+):
     """
     Compute the loss (solve the PDE) for everywhere not
     on a boundary or an initial condition.
     Args :
         q : primitive variables vector
         grad_q : gradient of the primitive variables [dx,dt]
+        hessian : second order derivatives with respect to input
+        artificial_viscosity: add viscosity to ensure non entropy violating shocks
     Returns :
         difference of pde from 0 squared
     """
@@ -120,12 +127,15 @@ def interior_loss(q: Tensor, grad_q: Tensor, grad_f: Tensor):
 
     rt = grad_q[:, 0, 1]
     rx = grad_q[:, 0, 0]
+    rxx = hessian[:, 0, 0]
 
     mt = grad_q[:, 1, 1]
     mx = grad_q[:, 1, 0]
+    mxx = hessian[:, 1, 0]
 
     et = grad_q[:, 2, 1]
     ex = grad_q[:, 2, 0]
+    exx = hessian[:, 1, 0]
 
     frt = grad_f[:, 0, 1]
     frx = grad_f[:, 0, 0]
@@ -136,13 +146,12 @@ def interior_loss(q: Tensor, grad_q: Tensor, grad_f: Tensor):
     fet = grad_f[:, 2, 1]
     fex = grad_f[:, 2, 0]
 
-
     # Note, the equations below are multiplied by r to reduce the loss.
     r_eq = torch.stack(
         [
-            rt + frx,
-            mt+fmx,
-            et+fex,
+            rt + frx + artificial_viscosity * rxx,
+            mt + fmx + artificial_viscosity * mxx,
+            et + fex + artificial_viscosity * exx,
         ]
     )
 
@@ -151,7 +160,15 @@ def interior_loss(q: Tensor, grad_q: Tensor, grad_f: Tensor):
     return res
 
 
-def euler_loss(x: Tensor, q: Tensor, grad_q: Tensor, grad_f: Tensor, targets: Tensor):
+def euler_loss(
+    x: Tensor,
+    q: Tensor,
+    grad_q: Tensor,
+    grad_f: Tensor,
+    hessian: Tensor,
+    artificial_viscosity: float,
+    targets: Tensor,
+):
     """
     Compute the loss for the euler equations
 
@@ -159,6 +176,8 @@ def euler_loss(x: Tensor, q: Tensor, grad_q: Tensor, grad_f: Tensor, targets: Te
         x : netork inputs (positions and time).
         q : primitive variable vector.
         grad_q : gradients of primitive values [dx, dt]
+        hessian : hessian with respect to inputs
+        artificial_viscosity: add viscosity to prevent entropy violating shocks
         targets : target values (used for boundaries and initial conditions.)
     Returns :
         sum of losses.
@@ -172,7 +191,13 @@ def euler_loss(x: Tensor, q: Tensor, grad_q: Tensor, grad_f: Tensor, targets: Te
     ic_indexes = torch.nonzero(ic_mask)
     interior = (torch.logical_not(left_mask + right_mask + ic_mask),)
 
-    in_loss = interior_loss(q[interior], grad_q[interior], grad_f[interior])
+    in_loss = interior_loss(
+        q[interior],
+        grad_q[interior],
+        grad_f[interior],
+        hessian=hessian[interior],
+        artificial_viscosity=artificial_viscosity,
+    )
     ic_loss = initial_condition_loss(q[ic_indexes], targets[ic_indexes])
     left_bc_loss = left_dirichlet_bc_loss(q[left_indexes], targets[left_indexes])
     right_bc_loss = right_dirichlet_bc_loss(q[right_indexes], targets[right_indexes])

neural_network_pdes/euler_networks.py
@@ -14,18 +14,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 from torch import Tensor
-from torch.nn import LazyBatchNorm1d
+from torch.nn import LazyBatchNorm1d, LazyInstanceNorm1d, LayerNorm
 import io
 import PIL.Image
 from torchvision import transforms
-from functorch import vmap, jacrev
+from functorch import vmap, jacrev, hessian
+from functorch.experimental import replace_all_batch_norm_modules_
 
 from neural_network_pdes.euler import pde_grid, PDEDataset, euler_loss
 
 import neural_network_pdes.euler as pform
 import neural_network_pdes.euler_conservative as cform
 
-from neural_network_pdes.transform_network import transform_mlp
+from neural_network_pdes.transform_network import transform_mlp, ReshapeNormalize
 import logging
 
 logger = logging.getLogger(__name__)
@@ -77,6 +78,7 @@ class Net(LightningModule):
                 hidden_segments=cfg.mlp.hidden.segments,
                 normalization=None if cfg.mlp.normalize is False else LazyBatchNorm1d,
                 non_linearity=nl,
+                periodicity=cfg.mlp.periodicity,
             )
         elif cfg.mlp.style == "transform":
             self.model = transform_mlp(
@@ -92,7 +94,9 @@ class Net(LightningModule):
                 hidden_segments=cfg.mlp.hidden.segments,
                 out_segments=cfg.mlp.output.segments,
                 hidden_layers=cfg.mlp.hidden.layers,
-                normalize=cfg.mlp.normalize,
+                normalization=None
+                if cfg.mlp.normalize is False
+                else ReshapeNormalize,  # LazyInstanceNorm1d,
                 scale=cfg.mlp.scale,
                 periodicity=cfg.mlp.periodicity,
             )
@@ -101,24 +105,26 @@ class Net(LightningModule):
                 f"Style should be 'standard' or 'transform', got {cfg.mlp.style}"
             )
 
+        # replace_all_batch_norm_modules_(self.model)
+
         self.root_dir = f"{hydra.utils.get_original_cwd()}"
         self.loss = nn.MSELoss()
 
     def forward(self, x):
         return self.model(x)
 
-    def flux(self, x) :
+    def flux(self, x):
         return cform.flux(self.forward(x), gamma=self._gamma)
 
     def setup(self, stage: int):
-        if self.cfg.form =="conservative" :
+        if self.cfg.form == "conservative":
             self.train_dataset = cform.PDEDataset(
                 size=self.cfg.data_size, rotations=self.cfg.rotations
             )
             self.test_dataset = cform.PDEDataset(
                 size=self.cfg.data_size, rotations=self.cfg.rotations
             )
-        else :
+        else:
             self.train_dataset = pform.PDEDataset(
                 size=self.cfg.data_size, rotations=self.cfg.rotations
             )
@@ -130,25 +136,33 @@ class Net(LightningModule):
         optimizer = self.optimizers()
 
         x, y = batch
+
         x.requires_grad_(True)
         y_hat = self(x)
 
         xf = x.reshape(x.shape[0], 1, x.shape[1])
         jacobian = vmap(jacrev(self.forward))(xf)
         nj = jacobian.reshape(-1, 3, 2)
-        
-        if self.cfg.form == "conservative" :
-            flux_jacobian = vmap(jacrev(self.flux))(xf).reshape(-1,3,2)
-            
+
+        if self.cfg.form == "conservative":
+            flux_jacobian = vmap(jacrev(self.flux))(xf).reshape(-1, 3, 2)
+            hess = vmap(hessian(self.forward))(xf).reshape(-1, 3, 4)
+
             in_loss, ic_loss, left_bc_loss, right_bc_loss = cform.euler_loss(
-                x=x, q=y_hat, grad_q=nj, grad_f=flux_jacobian, targets=y
+                x=x,
+                q=y_hat,
+                grad_q=nj,
+                grad_f=flux_jacobian,
+                hessian=hess,
+                artificial_viscosity=self.cfg.physics.artificial_viscosity,
+                targets=y,
             )
-        elif self.cfg.form =="primitive" :
-            
+        elif self.cfg.form == "primitive":
+
             in_loss, ic_loss, left_bc_loss, right_bc_loss = pform.euler_loss(
                 x=x, q=y_hat, grad_q=nj, targets=y
             )
-        else :
+        else:
             raise ValueError(f"form should be conservative or primitive")
 
         loss = in_loss + ic_loss + left_bc_loss + right_bc_loss

neural_network_pdes/transform_network.py
@@ -40,6 +40,17 @@ def fixed_rotation_layer(n: int):
     return layer
 
 
+class ReshapeNormalize(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.norm = torch.nn.LazyInstanceNorm1d(track_running_stats=False)
+
+    def __call__(self, x):
+        y = x.unsqueeze(1)
+        ans = self.norm(y).squeeze(1)
+        return ans
+
+
 def transform_mlp(
     layer_type: str,
     in_width: int,
@@ -55,7 +66,7 @@ def transform_mlp(
     hidden_layers: int,
     scale: float,
     periodicity: float,
-    normalize: bool = False,
+    normalization: torch.nn.Module,
 ) -> torch.nn.Module:
 
     fixed_input = fixed_rotation_layer(n=in_width)
@@ -73,7 +84,7 @@ def transform_mlp(
         hidden_width=hidden_width,
         hidden_layers=hidden_layers,
         hidden_segments=hidden_segments,
-        normalization=None if normalize is False else torch.nn.LazyBatchNorm1d,
+        normalization=normalization,
         scale=scale,
         periodicity=periodicity,
     )

tests/test_loss.py
@@ -9,16 +9,18 @@ from neural_network_pdes.euler import (
 from neural_network_pdes.transform_network import fixed_rotation_layer
 import torch
 
+
 def test_poly_convolution_2d_produces_correct_sizes():
 
     assert True is True
 
-def test_transform_network() :
-    
+
+def test_transform_network():
+
     ans = fixed_rotation_layer(2)
     res = torch.tensor(ans.weight)
-    assert res.shape == torch.Size([4,2])
+    assert res.shape == torch.Size([4, 2])
 
     ans = fixed_rotation_layer(3)
     res = torch.tensor(ans.weight)
-    assert res.shape == torch.Size([9,3])
+    assert res.shape == torch.Size([9, 3])